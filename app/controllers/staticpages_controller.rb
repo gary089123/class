4b9a0e95f8a86cdda9e8f0b01c8675cf289ded6a
Hsu <edittye379@gmail.com>
2016-08-31 00:57:29 +0800

    借用狀態頁面css

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -8,7 +8,12 @@ class StaticpagesController < ApplicationController
   end
 
   def status
-    @vertify_calendar_url='akpgt98gj0d9a53r7d7lj94kvc@group.calendar.google.com'
+    @vertify_202='akpgt98gj0d9a53r7d7lj94kvc@group.calendar.google.com'
+    @vertify_210='jnp6sauodn4e4v5hagn8sog4i4@group.calendar.google.com'
+    @vertify_002='sbbin0bcvk7c5r5ckle6nt4b88@group.calendar.google.com'
+    @rent_202='rr7j9s4d5k21c4bcicn50ccpv8@group.calendar.google.com'
+    @rent_210='iv6su9k0daaod3i3vgia1lfktc@group.calendar.google.com'
+    @rent_002='g2cjaf2h5p45gl29lpbseuan2o@group.calendar.google.com'
   end
 
 end
