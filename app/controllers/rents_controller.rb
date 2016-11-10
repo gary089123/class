c289ccefd9b169f1fcd54981587d1c68dd152e0a
Hsu <edittye379@gmail.com>
2016-11-11 06:12:22 +0800

    臨時性預約檢查是否時間重複

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -6,16 +6,18 @@ class RentsController < ApplicationController
   require 'json'
 
   def index
-    @rent_202 = ENV['rent_202']
-    @rent_210 = ENV['rent_210']
-    @rent_002 = ENV['rent_002']
+    # 臨時性的時間列出 /rents
+    @rent_202 = Rent.joins(:semester).where('semesters.is_open', true).where(facility: 4)
+    @rent_210 = Rent.joins(:semester).where('semesters.is_open', true).where(facility: 5)
+    @rent_002 = Rent.joins(:semester).where('semesters.is_open', true).where(facility: 6)
+    @rent_202 = @rent_202.order("semester_id asc")
+    @rent_210 = @rent_210.order("semester_id asc")
+    @rent_002 = @rent_002.order("semester_id asc")
   end
 
   def search
     @rent=Rent.where(user_id: current_user.id)
     @srent=Srent.where(user_id: current_user.id)
-    # abort @rent_times.inspect
-    # abort @rent.inspect
   end
 
   def show
@@ -53,9 +55,13 @@ class RentsController < ApplicationController
   end
 
   def destroy
-    puts @rent.facility
-    url = ENV['api_rent'] + @rent.apid.to_s + '?access_token='+ENV['access_token']
-    api = RestClient.delete(url , {})
+    ## API - 刪除rent
+    # url = ENV['api_rent'] + @rent.apid.to_s + '?access_token='+ENV['access_token']
+    # api = RestClient.delete(url , {})
+
+    # 刪除臨時性借用的時間
+    @rent.rent_times.destroy_all
+    # 刪除臨時性借用
     @rent.destroy
     redirect_to :back
   end
@@ -68,78 +74,92 @@ class RentsController < ApplicationController
     @rent=Rent.new
     @srent=Srent.new
     @semesters = Semester.where(is_open: true)
-    # abort @semesters.inspect
   end
 
   def create
-    # abort params[:rent].inspect
-    # @rent = Rent.new(params_rent)
-    @rent = Rent.new
-    @rent.semester_id = params[:rent][:semester_id]
-    @rent.name = params[:rent][:name]
-    @rent.teacher = params[:rent][:teacher]
-    @rent.phone = params[:rent][:phone]
-    @rent.email = params[:rent][:email]
-    @rent.facility = params[:rent][:facility]
+
+    rent = params[:rent]
+    @rent = Rent.new(params_rent)
     @rent.user_id = current_user.id
     @rent.status = "待審核"
-    # abort @rent.inspect
     @rent.save
 
+    # for api time spans
     data = [];
-    rent = params[:rent]
+    # 計算有幾個時間區間, default is 1
     num = rent[:calculus].to_i
-    if num > 0
-      i = 0
-      while i < num do
-        if (rent["year"+i.to_s]!=nil && rent["month"+i.to_s]!=nil && rent["day"+i.to_s]!=nil && rent["start_time"+i.to_s]!=nil \
-          && rent["year"+i.to_s]!=nil && rent["month"+i.to_s]!=nil && rent["day"+i.to_s]!=nil && rent["end_time"+i.to_s]!=nil)
 
-          start = Time.new(rent["year"+i.to_s].to_i ,rent["month"+i.to_s].to_i ,rent["day"+i.to_s].to_i ,rent["start_time"+i.to_s].to_i, 0)
-          endt = Time.new(rent["year"+i.to_s].to_i ,rent["month"+i.to_s].to_i ,rent["day"+i.to_s].to_i ,rent["end_time"+i.to_s].to_i, 0)
+    # loop every time spans
+    i = 0
+    while i < num do
+      if (rent["year"+i.to_s]!=nil && rent["month"+i.to_s]!=nil && rent["day"+i.to_s]!=nil && rent["start_time"+i.to_s]!=nil \
+        && rent["year"+i.to_s]!=nil && rent["month"+i.to_s]!=nil && rent["day"+i.to_s]!=nil && rent["end_time"+i.to_s]!=nil)
 
-          # rent_time 資料表
-          @rent_time = RentTime.new
-          @rent_time.rent_id = @rent.id
-          @rent_time.start = start
-          @rent_time.end = endt
-          @rent_time.save
+        # 時間格式轉換
+        start = Time.new(rent["year"+i.to_s].to_i ,rent["month"+i.to_s].to_i ,rent["day"+i.to_s].to_i ,rent["start_time"+i.to_s].to_i, 0)
+        endt = Time.new(rent["year"+i.to_s].to_i ,rent["month"+i.to_s].to_i ,rent["day"+i.to_s].to_i ,rent["end_time"+i.to_s].to_i, 0)
 
-          # api
-          h = { :start => start ,:end => endt }
-          data << h
+        # 結束時間當然要大於開始時間
+        if !(start < endt)
+          @rent.rent_times.destroy_all
+          @rent.destroy
+          return redirect_to :back, notice: '結束時間必須大於開始時間'
         end
 
-        i += 1
+        # 時間應該介於 這學期的start ~ end
+        if !( @rent.semester.start <= start && endt <= @rent.semester.end )
+          @rent.rent_times.destroy_all
+          @rent.destroy
+          return redirect_to :back, notice: '時間未在您輸入的學期內'
+        end
+
+        # 檢查是否跟臨時性重疊
+        @all_rents = Rent.where(semester_id: rent[:semester_id], facility: rent[:facility])
+        @all_rents.each do |all_rent|
+          all_rent.rent_times.each do |rent_time|
+            if check_conflict?( [start, endt], [rent_time.start,rent_time.end] )
+              @rent.rent_times.destroy_all
+              @rent.destroy
+              return redirect_to :back, notice: '該時段已被申請'
+            end
+          end
+        end
+
+        # TODO: 檢查是否跟學期性撞時
+
+        # rent_time 資料表
+        @rent_time = RentTime.new
+        @rent_time.rent_id = @rent.id
+        @rent_time.start = start
+        @rent_time.end = endt
+        @rent_time.save
+
+        # for api
+        h = { :start => start ,:end => endt }
+        data << h
       end
+
+      i += 1
     end
-    # abort data.inspect
 
+    ## API - 新增rent
+    # jdata=data.to_json
+    # url = ENV['api_facility'] + @rent.facility.to_s + '/rent'
+    # api = RestClient.post(url,
+    # {
+    #   :name => @rent.name,
+    #   :spans => jdata,
+    #   :access_token => ENV['access_token']
+    # })
+    # japi=JSON.parse(api)
 
-    # rent = params[:rent]
-    # start = DateTime.new(rent["start(1i)"].to_i ,rent["start(2i)"].to_i ,rent["start(3i)"].to_i ,rent["start(4i)"].to_i, rent["start(5i)"].to_i)
-    # endt = DateTime.new(rent["start(1i)"].to_i ,rent["start(2i)"].to_i ,rent["start(3i)"].to_i ,rent["end(4i)"].to_i, rent["end(5i)"].to_i)
-    # data = [
-    #   {
-    #     :start => start,
-    #     :end => endt
-    #   }
-    # ]
+    # 取得api的rent id以備更新或刪除
+    # @rent.apid = japi["id"].to_i
+    # @rent.save
 
-    jdata=data.to_json
-    url = ENV['api_facility'] + @rent.facility.to_s + '/rent'
-    api = RestClient.post(url,
-    {
-      :name => @rent.name,
-      :spans => jdata,
-      :access_token => ENV['access_token']
-    })
-    japi=JSON.parse(api)
-    puts japi["id"]
-    @rent.apid = japi["id"].to_i
+    # 意義不明?
+    # params[:format]=nil
 
-    @rent.save
-    params[:format]=nil
     redirect_to rent_print_path
   end
 
@@ -163,8 +183,7 @@ class RentsController < ApplicationController
   end
 
   def params_rent
-    params.require(:rent)#.permit(:facility,:name,:start,:end)
-
+    params.require(:rent).permit(:semester_id,:name,:teacher,:phone,:email,:facility)
   end
 
 end
